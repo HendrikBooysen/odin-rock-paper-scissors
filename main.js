75ca23b24d68e90537806d75fa86994959a5ed2a
hendrik <24483010+kwagga333@users.noreply.github.com>
2023-02-06 21:17:03 +0200

    Game func added and for loop

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-
-
+let answer = (prompt("Please pick Rock / Paper / Scissors: "));
+let playerScore = 0;
+let computerScore = 0;
 
 //Function to random return choice in array to mimic Computer playing
 function getComputerChoice(){
 
     let choice = ["Rock","Paper","Scissors"];
-    let radomNumber = Math.floor(Math.random() * choice.length);
+    let randomNumber = Math.floor(Math.random() * choice.length);
 
-    return(choice[radomNumber]);
+    return(choice[randomNumber]);
 }
 
 //Function to mimic single round of play and give text out-put of results
@@ -15,18 +16,28 @@ function playRound(playerSelection, computerSelection){
 
    if(playerSelection == computerSelection){
    
-   console.log("It's a Draw.!");
+   return("It's a Draw.!");
 }else if(playerSelection == "Rock" && computerSelection == "Scissors"){
-    console.log("Computer lose! Rock beats Scissors.!")
+    return("Computer lose! Rock beats Scissors.!");
 }else if(playerSelection == "Scissors" && computerSelection == "Paper"){
-    console.log("Computer lose! Scissors beats Paper.!");
+    return("Computer lose! Scissors beats Paper.!");
 }else if(playerSelection == "Paper" && computerSelection == "Rock"){
-    console.log("Computer lose! Paper beats Rock.!");
+    return("Computer lose! Paper beats Rock.!");
 }else if(computerSelection == "Rock" && playerSelection == "Scissors"){
-    console.log("You Lose! Rock beats Scissors.!")
+    return("You Lose! Rock beats Scissors.!");
 }else if(computerSelection == "Scissors" && playerSelection == "Paper"){
-    console.log("You lose! Scissors beats Paper.!");
+    return("You lose! Scissors beats Paper.!");
 }else if(computerSelection == "Paper" && playerSelection == "Rock"){
-    console.log("You lose! Paper beats Rock.!");
+    return("You lose! Paper beats Rock.!");
 }
 }
+
+function game(){
+
+    for(let i = 0; i < 5; i++){
+        console.log(playRound(answer, getComputerChoice()));
+    }  
+}
+
+
+  
