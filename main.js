5ca666d3f36cb3b219c022aebdbdfc926f988703
hendrik <24483010+kwagga333@users.noreply.github.com>
2023-02-06 13:48:37 +0200

    Add function getComputerChoice()

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+
+
+
+//Function to random return choice in array to mimic Computer playing
+function getComputerChoice(){
+
+    let choice = ["Rock","Paper","Scissors"];
+    let radomNumber = Math.floor(Math.random() * choice.length);
+
+    console.log(choice[radomNumber]);
+};
