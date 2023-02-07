8aca8aeb3eee2cfce5474c7cc2e203b0ea235d66
hendrik <24483010+kwagga333@users.noreply.github.com>
2023-02-07 12:16:19 +0200

    ADD FOR loop at Game func

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let answer = (prompt("Please pick Rock / Paper / Scissors: "));
 let playerScore = 0;
 let computerScore = 0;
+let drawScore = 0;
 
 //Function to random return choice in array to mimic Computer playing
 function getComputerChoice(){
@@ -15,29 +15,41 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection){
 
    if(playerSelection == computerSelection){
-   
+   drawScore++;
    return("It's a Draw.!");
 }else if(playerSelection == "Rock" && computerSelection == "Scissors"){
+    playerScore++;
     return("Computer lose! Rock beats Scissors.!");
 }else if(playerSelection == "Scissors" && computerSelection == "Paper"){
+    playerScore++;
     return("Computer lose! Scissors beats Paper.!");
 }else if(playerSelection == "Paper" && computerSelection == "Rock"){
+    playerScore++;
     return("Computer lose! Paper beats Rock.!");
 }else if(computerSelection == "Rock" && playerSelection == "Scissors"){
+    computerScore++;
     return("You Lose! Rock beats Scissors.!");
 }else if(computerSelection == "Scissors" && playerSelection == "Paper"){
+    computerScore++;
     return("You lose! Scissors beats Paper.!");
 }else if(computerSelection == "Paper" && playerSelection == "Rock"){
+    computerScore++;
     return("You lose! Paper beats Rock.!");
 }
 }
-
+// Function with a FOR LOOP to mimic the 5 rounds of a game 
 function game(){
 
-    for(let i = 0; i < 5; i++){
-        console.log(playRound(answer, getComputerChoice()));
+    for(let i = 1; i <= 5; i++){
+    let answer = (prompt("Please pick Rock / Paper / Scissors: "));
+    console.log(playRound(answer, getComputerChoice())); 
     }  
 }
 
+game();
+
+console.log('This is your Score:' + playerScore);
+console.log('This is the Computer Score:' + computerScore);
+//console.log(drawScore);
 
   
